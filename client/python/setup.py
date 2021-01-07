b7c77897b6027f4cdd447171ab3a2cf2937c79f6
You Jhin <73587140+uJhin@users.noreply.github.com>
2021-01-07 16:11:45 +0900

    Create setup.py
    Upbit OPEN API Version: 1.1.6

---
+++ client/python/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='upbit_client',
+    version='1.1.6',
+    packages=find_packages(),
+    python_requires='>=3',
+    keywords=[
+        'Upbit',
+        'upbit',
+        'upbit-client',
+        'Upbit-Client',
+        'Upbit_client',
+        'upbit_client'
+    ],
+    url='https://github.com/uJhin/upbit-client',
+    license='MIT License',
+    author='ujhin',
+    author_email='ujhin942@gmail.com',
+    description='Upbit OPEN API Client'
+)
