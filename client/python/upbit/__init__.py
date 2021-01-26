3c6ca848b194aabe7081d241bf621324814ae1f6
uJhin <ujhin942@gmail.com>
2021-01-27 01:36:01 +0900

    Update Version 1.1.6.25

--- client/python/upbit/__init__.py
+++ client/python/upbit/__init__.py
@@ -7,7 +7,7 @@ from upbit.utils import Version
 __all__ = ['client', 'websocket']
 __module_name__ = 'upbit-client'
 
-__version__ = '1.1.6.24a'
+__version__ = '1.1.6.25'
 __released_version__ = Version.get_versions(__module_name__)
 __latest_version__ = __released_version__[0]
 
