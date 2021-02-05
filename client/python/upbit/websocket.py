d60fa413271e6840d96ba9a8c50ff3bef0f458ff
uJhin <ujhin942@gmail.com>
2021-02-05 17:26:33 +0900

    Update Version 1.1.6.28

--- client/python/upbit/websocket.py
+++ client/python/upbit/websocket.py
@@ -48,28 +48,53 @@ class UpbitWebSocket:
         return codes
 
     @staticmethod
-    def generate_payload(
+    def generate_type_field(
         type: str,
         codes: Union[List[str]],
         isOnlySnapshot: bool = None,
         isOnlyRealtime: bool = None,
-        format: str = 'DEFAULT',
-        ticket: str = None
     ) -> str:
         """
-        :param type: 수신할 시세 타입 (현재가: ticker, 체결: trade, 호가: orderbook)
+        :param type: 수신할 시세 타입 (현재가: `ticker`, 체결: `trade`, 호가: `orderbook`)
         :type type: str
 
         :param codes: 수신할 시세 종목 정보
-        :type codes: list[str,]
+        :type codes: list[str, ...]
 
         :param isOnlySnapshot: 시세 스냅샷만 제공 여부
         :type isOnlySnapshot: bool
 
         :param isOnlyRealtime: 실시간 시세만 제공 여부
         :type isOnlyRealtime: bool
+        """
+        field = {}
 
-        :param format: 포맷 (SIMPLE: 간소화된 필드명, DEFAULT (생략 가능))
+        if type in ['ticker', 'trade', 'orderbook']:
+            field["type"] = type
+        else:
+            raise ValueError("'type' is not available")
+
+        codes = [code.upper() for code in codes]
+        field["codes"] = codes
+
+        if isOnlySnapshot is not None:
+            field["isOnlySnapshot"] = isOnlySnapshot
+        if isOnlyRealtime is not None:
+            field["isOnlyRealtime"] = isOnlyRealtime
+
+        return field
+
+    @staticmethod
+    def generate_payload(
+        type_fields: Union[List[dict]],
+        ticket: str = None,
+        format: str = 'DEFAULT'
+    ):
+        """
+        :param type_fields: Type Fields
+        :type type_fields: list[dict, ...]
+
+        :param format: 포맷 (`SIMPLE`: 간소화된 필드명, `DEFAULT`: 기본값 (생략 가능))
         :type format: str
 
         :param ticket: 식별값
@@ -80,21 +105,11 @@ class UpbitWebSocket:
         ticket = ticket if ticket else str(uuid.uuid4())
         payload.append({"ticket": ticket})
 
-        field = {}
-        if type in ['ticker', 'trade', 'orderbook']:
-            field["type"] = type
-        else:
-            raise ValueError("'type' is not available")
-        codes = [code.upper() for code in codes]
-        field["codes"] = codes
-        if isOnlySnapshot is not None:
-            field["isOnlySnapshot"] = isOnlySnapshot
-        if isOnlyRealtime is not None:
-            field["isOnlyRealtime"] = isOnlyRealtime
-        payload.append(field)
+        payload.extend(type_fields)
 
-        format = format.upper() if format.upper() in ['SIMPLE', 'DEFAULT'] else 'DEFAULT'
-        payload.append({"format": format})
+        fmt = format.upper()
+        fmt = fmt if fmt in ['SIMPLE', 'DEFAULT'] else 'DEFAULT'
+        payload.append({"format": fmt})
 
         return json.dumps(payload)
 
