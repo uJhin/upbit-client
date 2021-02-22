a923bcc2fab23507cc149fd0dc9f5d17aa4dfa85
uJhin <ujhin942@gmail.com>
2021-02-23 00:01:27 +0900

    Update Setup codes
    Update Setup codes

--- client/python/setup.py
+++ client/python/setup.py
@@ -11,33 +11,33 @@ with open('README.rst') as readme:
 
 
 setup(
-    name='upbit_client',
-    version=version,
-    packages=find_packages(),
-    install_requires=[
+    name             = 'upbit_client',
+    version          = version,
+    packages         = find_packages(),
+    install_requires = [
         'bravado>=11.0.2',
         'PyJWT>=2.0',
         'websockets>=8.1'
     ],
-    extras_require={
+    extras_require  = {
         'fido': [
             'fido>=4.2.1'
         ]
     },
-    python_requires='>=3.8',
-    keywords=[
+    python_requires = '>=3.8',
+    keywords        = [
         'Upbit',
         'upbit',
         'upbit-client',
         'Upbit-Client',
         'Upbit_client'
     ],
-    url='https://github.com/uJhin/upbit-client',
-    download_url='https://github.com/uJhin/upbit-client/releases',
-    license='MIT License',
-    author='ujhin',
-    author_email='ujhin942@gmail.com',
-    description='Upbit OPEN API Client',
-    long_description_content_type='text/x-rst',
-    long_description=long_description
+    url                           = 'https://github.com/uJhin/upbit-client',
+    download_url                  = 'https://github.com/uJhin/upbit-client/releases',
+    license                       = 'MIT License',
+    author                        = 'ujhin',
+    author_email                  = 'ujhin942@gmail.com',
+    description                   = 'Upbit OPEN API Client',
+    long_description_content_type = 'text/x-rst',
+    long_description              = long_description
 )
