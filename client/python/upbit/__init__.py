2883b090483f78ccf972057f0db345ab467ddc44
uJhin <ujhin942@gmail.com>
2021-04-01 10:27:22 +0900

    Add Package Docstring

--- client/python/upbit/__init__.py
+++ client/python/upbit/__init__.py
@@ -1,3 +1,16 @@
+"""
+[Upbit Client]
+
+Please read the official Upbit Client document.
+Documents: https://ujhin.github.io/upbit-client-docs/
+
+- Upbit OPEN API Version: 1.2.0
+- Author: ujhin
+- Email: ujhin942@gmail.com
+- GitHub: https://github.com/uJhin
+- Official OPEN API Documents: https://docs.upbit.com
+- Official Support Email: open-api@upbit.com
+"""
 
 from . import pkginfo
 
