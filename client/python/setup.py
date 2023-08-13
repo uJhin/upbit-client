fa141cfc360b1219448f99c177e9965f3525b1eb
seunggabi <seunggabi@naver.com>
2023-08-13 23:01:25 +0900

    fix: requests

--- client/python/setup.py
+++ client/python/setup.py
@@ -1,38 +1,36 @@
-
 from setuptools import setup
 from setuptools import find_packages
 
 from upbit import pkginfo
 
-
 with open('README.rst') as readme:
     long_description = readme.read()
 
-
 setup(
-    name     = pkginfo.PACKAGE_NAME,
-    version  = pkginfo.CURRENT_VERSION,
-    packages = find_packages(),
-    install_requires = [
-          'bravado>=11.0.2'
+    name=pkginfo.PACKAGE_NAME,
+    version=pkginfo.CURRENT_VERSION,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'bravado>=11.0.2'
         , 'PyJWT>=2.4.0'
         , 'websockets>=10.3'
     ],
-    extras_require  = {
+    extras_require={
         'fido': [
             'fido>=4.2.1'
         ]
     },
-    python_requires = '>=3.8',
-    classifiers     = [
-          'Programming Language :: Python :: 3.8'
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3.8'
         , 'Programming Language :: Python :: 3.9'
         , 'Programming Language :: Python :: 3.10'
         , 'Programming Language :: Python :: 3.11'
         , 'Programming Language :: Python :: 3.12'
     ],
-    keywords        = [
-          'Upbit'
+    keywords=[
+        'Upbit'
         , 'upbit'
         , 'upbit-client'
         , 'Upbit-Client'
@@ -42,12 +40,12 @@ setup(
         , 'Upbit_api_connector'
         , 'upbit_api_connector'
     ],
-    url                           = 'https://github.com/uJhin/upbit-client',
-    download_url                  = 'https://github.com/uJhin/upbit-client/releases',
-    license                       = 'MIT License',
-    author                        = 'ujhin',
-    author_email                  = 'ujhin942@gmail.com',
-    description                   = 'Upbit OPEN API Client',
-    long_description_content_type = 'text/x-rst',
-    long_description              = long_description
+    url='https://github.com/uJhin/upbit-client',
+    download_url='https://github.com/uJhin/upbit-client/releases',
+    license='MIT License',
+    author='ujhin',
+    author_email='ujhin942@gmail.com',
+    description='Upbit OPEN API Client',
+    long_description_content_type='text/x-rst',
+    long_description=long_description
 )
