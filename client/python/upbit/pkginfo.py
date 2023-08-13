a42c244b16ec0ffa31892cbd6bcf2ea4c273b8ce
seunggabi <seunggabi@naver.com>
2023-08-13 23:09:06 +0900

    fix: requests 3

--- client/python/upbit/pkginfo.py
+++ client/python/upbit/pkginfo.py
@@ -12,33 +12,7 @@ Documents: https://ujhin.github.io/upbit-client-docs/
 - Official Support Email: open-api@upbit.com
 """
 
-import logging
-import requests
-
-from distutils.version import LooseVersion
-
-
-def _get_versions(package_name):
-    url = f"https://pypi.org/pypi/{package_name}/json"
-    resp = requests.get(url)
-    data = resp.json()
-    versions = data["releases"].keys()
-    return sorted(versions, key=LooseVersion, reverse=True)
-
-
-PACKAGE_NAME     = "upbit-client"
+PACKAGE_NAME = "upbit-client"
 
 OPEN_API_VERSION = "1.3.4"
-CURRENT_VERSION  = OPEN_API_VERSION+".1"
-
-# RELEASED_VERSION = _get_versions(PACKAGE_NAME)
-# LATEST_VERSION   = RELEASED_VERSION[0]
-
-
-# if LATEST_VERSION != CURRENT_VERSION:
-#     logging.basicConfig(format="[%(levelname)s] %(message)s")
-#     logging.warning(
-#         f"{PACKAGE_NAME} is currently a newer version: {LATEST_VERSION}\n"
-#         f"Please update to the latest version using the pip command:"
-#         f"`pip install --upgrade {PACKAGE_NAME}`"
-#     )
+CURRENT_VERSION = OPEN_API_VERSION + ".1"
